092b0235a72a240d1ffd39079f7ab8bf32d9981f
pdjshog <pdjshog@hotmail.com>
2021-07-15 15:12:43 +0400

    fixes to prod build

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,84 +1,92 @@
 let player = {
-  url: "https://pl-cache.weareone.world/minimal/nowplaying?stream=https://playerservices.streamtheworld.com/api/livestream-redirect/OWR_INTERNATIONAL_ADP.m3u8",
-  interval: null,
-  init: function () {
-    player.polling();
-    player.interval = setInterval(function () {
-      player.polling()
-    }, 60 * 1000);
-    player.getStream();
-  },
-  polling: function () {
-    try {
-      let xhr = new XMLHttpRequest();
-      xhr.open('GET', player.url, true);
-      xhr.onload = function (e) {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            let responseData = JSON.parse(xhr.response);
-            document.querySelector('.player-artist-heading').innerHTML = responseData.artist;
-            document.querySelector('.player-artist-title').innerHTML = responseData.title;
-          }
+    url: "https://pl-cache.weareone.world/minimal/nowplaying?stream=https://playerservices.streamtheworld.com/api/livestream-redirect/OWR_INTERNATIONAL_ADP.m3u8",
+    interval: null,
+    init: function () {
+        player.polling();
+        player.interval = setInterval(function () {
+            player.polling()
+        }, 60 * 1000);
+        player.getStream();
+
+        document.querySelector('#player').remote.watchAvailability(function (available) {
+            console.log(available ? "airplay" : "no remote play")
+            // document.getElementById("deviceBtn").style.display = available ? "inline" : "none";
+        }).catch((e) => {
+            console.log(e);
+            // document.getElementById("deviceBtn").style.display = "inline";
+        });
+    },
+    polling: function () {
+        try {
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', player.url, true);
+            xhr.onload = function (e) {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        let responseData = JSON.parse(xhr.response);
+                        document.querySelector('.player-artist-heading').innerHTML = responseData.artist;
+                        document.querySelector('.player-artist-title').innerHTML = responseData.title;
+                    }
+                }
+            };
+            xhr.send(null);
+        } catch (err) {
+            console.log(err);
         }
-      };
-      xhr.send(null);
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  streamGetUrl: 'https://opml.radiotime.com/Tune.ashx?id=s307749&render=json&listenId=1604657445',
-  getStream: function () {
-    try {
-      let xhr = new XMLHttpRequest();
-      xhr.open('GET', player.streamGetUrl, true);
-      xhr.onload = function (e) {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            let responseData = JSON.parse(xhr.response);
-            document.querySelector('audio').setAttribute('src', responseData.body[0].url);
-            document.querySelector('audio').play();
-          }
+    },
+    streamGetUrl: 'https://opml.radiotime.com/Tune.ashx?id=s307749&render=json&listenId=1604657445',
+    getStream: function () {
+        try {
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', player.streamGetUrl, true);
+            xhr.onload = function (e) {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        let responseData = JSON.parse(xhr.response);
+                        document.querySelector('#player').setAttribute('src', responseData.body[0].url);
+                        document.querySelector('#player').play();
+                    }
+                }
+            };
+            xhr.send(null);
+        } catch (err) {
+            console.log(err);
         }
-      };
-      xhr.send(null);
-    } catch (err) {
-      console.log(err);
     }
-  }
 
 }
 
 
 document.addEventListener("DOMContentLoaded", function (event) {
-  player.init();
+    player.init();
 
-  document.querySelector('audio').addEventListener("playing", function () {
-    document.querySelector('.player-button').classList.remove('player-button-spining');
-    document.querySelector('.player-button').classList.add('player-button-pause');
-  }, true);
-  document.querySelector('audio').addEventListener("loadstart", function () {
-    document.querySelector('.player-button').classList.remove('player-button-pause');
-    document.querySelector('.player-button').classList.add('player-button-spining');
-  }, true);
-  document.querySelector('audio').addEventListener("pause", function () {
-    document.querySelector('.player-button').classList.remove('player-button-spining');
-    document.querySelector('.player-button').classList.remove('player-button-pause');
-  }, true);
+    document.querySelector('#player').addEventListener("playing", function () {
+        document.querySelector('.player-button').classList.remove('player-button-spining');
+        document.querySelector('.player-button').classList.add('player-button-pause');
+    }, true);
+    document.querySelector('#player').addEventListener("loadstart", function () {
+        document.querySelector('.player-button').classList.remove('player-button-pause');
+        document.querySelector('.player-button').classList.add('player-button-spining');
+    }, true);
+    document.querySelector('#player').addEventListener("pause", function () {
+        document.querySelector('.player-button').classList.remove('player-button-spining');
+        document.querySelector('.player-button').classList.remove('player-button-pause');
+    }, true);
 
-  document.querySelector('.player-button').addEventListener("click", function () {
-    if (document.querySelector('.player-button').classList.contains('player-button-pause')) {
-      document.querySelector('audio').pause();
-    } else {
-      document.querySelector('audio').play();
-    }
-  });
+    document.querySelector('.player-button').addEventListener("click", function () {
+        if (document.querySelector('.player-button').classList.contains('player-button-pause')) {
+            document.querySelector('#player').pause();
+        } else {
+            document.querySelector('#player').play();
+        }
+    });
 });
 
-const { ipcRenderer } = require('electron');
+const {ipcRenderer} = require('electron');
 ipcRenderer.on('play', (event) => {
-  document.querySelector('audio').play();
+    document.querySelector('#player').play();
 });
 
 ipcRenderer.on('pause', (event) => {
-  document.querySelector('audio').pause();
+    document.querySelector('#player').pause();
 })
--- index.js
+++ index.js
@@ -1,84 +1,109 @@
-const { app, BrowserWindow, Menu, Tray } = require('electron')
+const {nativeImage} = require("electron");
+const {app, BrowserWindow, Menu, Tray} = require('electron')
 
-function createWindow() {
-  const win = new BrowserWindow({
-    width: 450,
-    height: 85,
-    resizable: false,
-    autoHideMenuBar: true,
-    icon: 'assets/img/icon.png',
-    titleBarStyle: 'customButtonsOnHover',
-    alwaysOnTop: true,
-    webPreferences: {
-      nodeIntegration: true,
-      webSecurity: false
-    }
-  });
-
-  win.allowExit = false;
-  if (process.platform == 'darwin') {
-    app.dock.hide();
-  }
-
-  win.on('close', (event) => {
-    if (win.allowExit) {
-      return true;
-    }
-    event.preventDefault();
-    win.hide();
-  });
-
-  let tray = new Tray('assets/tray/icon.png');
-  const contextMenu = Menu.buildFromTemplate([
-    {
-      label: 'Play',
-      type: 'normal',
-      click: function (ecv) {
-        win.webContents.send('play');
-      }
-    },
-    {
-      label: 'Pause',
-      type: 'normal',
-      click: function (ecv) {
-        win.webContents.send('pause');
-      }
-    },
-    {
-      label: 'Toggle Player',
-      type: 'normal',
-      click: function (ecv) {
-        if (win.isVisible()) {
-          win.hide();
-        } else {
-          win.show();
+function createWindow(separator) {
+    const win = new BrowserWindow({
+        width: 450,
+        height: 85,
+        resizable: false,
+        autoHideMenuBar: true,
+        icon: 'assets/img/icon.png',
+        titleBarStyle: 'customButtonsOnHover',
+        alwaysOnTop: true,
+        webPreferences: {
+            nodeIntegration: true,
+            webSecurity: false
         }
-      }
-    },
-    {
-      label: '',
-      type: 'separator',
-    },
+    });
 
-    {
-      label: 'Exit',
-      type: 'normal',
-      click: function () {
-        win.allowExit = true;
-        app.quit()
-      }
+    win.allowExit = false;
+    if (process.platform == 'darwin') {
+        app.dock.hide();
     }
-  ])
-  tray.setToolTip('One World Radio');
-  tray.setContextMenu(contextMenu);
-  win.loadFile('index.html')
+
+    win.on('close', (event) => {
+        if (win.allowExit) {
+            return true;
+        }
+        event.preventDefault();
+        win.hide();
+    });
+
+    const dataPath =
+        process.env.NODE_ENV === 'development' ? '.' : process.resourcesPath;
+
+    let tray = new Tray(dataPath + '/assets/tray/icon.png');
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'Play',
+            type: 'normal',
+            click: function (ecv) {
+                win.webContents.send('play');
+            }
+        },
+        {
+            label: 'Pause',
+            type: 'normal',
+            click: function (ecv) {
+                win.webContents.send('pause');
+            }
+        },
+        {
+            label: 'Toggle Player',
+            type: 'normal',
+            click: function (ecv) {
+                if (win.isVisible()) {
+                    win.hide();
+                } else {
+                    win.show();
+                }
+            }
+        },
+        {
+            label: '',
+            type: 'separator',
+        },
+
+        {
+            label: 'Dev Tools',
+            type: 'normal',
+            click: function (ecv) {
+                win.webContents.openDevTools();
+            }
+        },
+        {
+            label: '',
+            type: 'separator',
+        },
+        {
+            label: 'Exit',
+            type: 'normal',
+            click: function () {
+                win.allowExit = true;
+                app.quit()
+            }
+        }
+    ])
+    tray.setToolTip('One World Radio');
+    tray.setContextMenu(contextMenu);
+    win.loadFile('index.html')
 }
 
-app.whenReady().then(createWindow)
+try {
+    app.whenReady()
+        .then(createWindow);
+} catch (e) {
+    alert(e);
+}
 
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
 })
+
+process.on('unhandledRejection', (reason, p) => {
+    console.error('Unhandled Rejection at:', p, 'reason:', reason)
+    process.exit(1)
+});
